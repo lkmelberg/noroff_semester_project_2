13f0e7d8b5d78a406be05e45fb4d0f4be4f7a169
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-05-20 13:16:53 +0200

    update avatar

--- js/modules/functions/user/changeAvatar.mjs
+++ js/modules/functions/user/changeAvatar.mjs
@@ -19,3 +19,56 @@
 //     console.log(error);
 //   }
 // }
+import {
+  token,
+  avatarReg,
+  updateAvatarURL,
+  updateAvatarForm,
+  displayMSG,
+} from "../../variables/variables.mjs";
+
+export function updateAvatar(e) {
+  // dont move from page
+  e.preventDefault();
+
+  // add input values
+  const updateContent = {
+    avatar: avatarReg.value,
+  };
+
+  async function update(url, data) {
+    try {
+      const postData = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      };
+      const response = await fetch(url, postData);
+      console.log(response);
+      const json = await response.json();
+      console.log(json);
+      displayMSG.innerHTML = `<div class="postSuccess">Avatar updated! Please wait</div>`;
+      updateAvatarForm.reset();
+      setTimeout(function () {
+        displayMSG.innerHTML = "";
+        location.reload();
+      }, 2500);
+      return json;
+    } catch (error) {
+      console.log(error);
+      displayMSG.innerHTML = `<div class="postError">Avatar not updated, Sorry about that. Try again or come back later :() </div>`;
+      updateAvatarForm.reset();
+      setTimeout(function () {
+        displayMSG.innerHTML = "";
+      }, 5000);
+    }
+  }
+
+  console.log(updateContent);
+  update(updateAvatarURL, updateContent);
+}
+
+// updateAvatarForm.addEventListener("submit", updateAvatar);
--- js/profile.mjs
+++ js/profile.mjs
@@ -1,4 +1,8 @@
 import { profileSpesificURL } from "./modules/variables/variables.mjs";
-import { fetchProfileInfo } from "./modules/functions/user/profileInfo.mjs";
+import {
+  fetchProfileInfo,
+  fetchProfileAvatar,
+} from "./modules/functions/user/profileInfo.mjs";
 
 fetchProfileInfo(profileSpesificURL);
+fetchProfileAvatar(profileSpesificURL);
