e99bc58bafbfd2035476e3c081c02ca81f897039
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-05-22 14:37:05 +0200

    added image gallery + styling

--- js/modules/functions/listings/getListings.mjs
+++ js/modules/functions/listings/getListings.mjs
@@ -5,6 +5,87 @@ import {
   token,
 } from "../../variables/variables.mjs";
 
+export function getListings(listing, listingsCont) {
+  for (let i = 0; i < listing.length; i++) {
+    const { id, title, description, seller, media } = listing[i];
+    const listingDate = listing[i].endsAt.substring(0, 10);
+    const listingTime = listing[i].endsAt.substring(11, 16);
+    const listingSeller = seller.name;
+    const listingAvatar = seller.avatar;
+    // const listingImg = media[0];
+
+    const listingCol = document.createElement("div");
+    listingCol.classList.add("col");
+
+    const listingCard = document.createElement("div");
+    listingCard.classList.add("card");
+
+    const imgCont = document.createElement("div");
+    imgCont.classList.add("card");
+
+    const imgRow = document.createElement("div");
+    imgRow.classList.add("row");
+
+    const imgCol = document.createElement("div");
+    imgCol.classList.add("cardGallery", "col-lg-4", "col-md-12", "mb-lg-0");
+
+    media.forEach((media) => {
+      const img = document.createElement("img");
+      img.classList.add("shadow-1-strong", "rounded");
+      img.setAttribute("alt", `${title} listing image`);
+      img.src = media;
+      imgCol.appendChild(img);
+    });
+
+    // const listingImgs = document.createElement("img");
+    // listingImgs.classList.add("img-thumbnail");
+    // listingImgs.setAttribute("alt", `${title} listing image`);
+    // listingImgs.src = listingImg;
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const h5Title = document.createElement("h5");
+    h5Title.classList.add("card-title");
+    h5Title.textContent = `Item: ${title}`;
+
+    const pDesc = document.createElement("p");
+    pDesc.classList.add("card-text");
+    pDesc.textContent = `Description: ${description}`;
+
+    const pDeadline = document.createElement("p");
+    pDeadline.textContent = `Deadline: ${listingDate} at ${listingTime}`;
+
+    const pSeller = document.createElement("p");
+    pSeller.textContent = `Seller: ${listingSeller} `;
+
+    const sellerAvatar = document.createElement("img");
+    sellerAvatar.classList.add("listingAvatar");
+    sellerAvatar.setAttribute("alt", `${listingSeller}'s avatar`);
+    sellerAvatar.src = listingAvatar;
+
+    const bidLink = document.createElement("a");
+    bidLink.classList.add("btn", "btn-primary");
+    bidLink.href = `listing.html?id=${id}`;
+    bidLink.textContent = "See bids / Add bid";
+
+    listingsCont.appendChild(listingCol);
+    listingCol.appendChild(listingCard);
+    listingCard.appendChild(imgCont);
+    imgCont.appendChild(imgRow);
+    imgRow.appendChild(imgCol);
+    listingCard.appendChild(cardBody);
+    cardBody.appendChild(h5Title);
+    cardBody.appendChild(pDesc);
+    cardBody.appendChild(pDeadline);
+    cardBody.appendChild(pSeller);
+    cardBody.appendChild(bidLink);
+    pSeller.appendChild(sellerAvatar);
+  }
+
+  loader.classList.add("hide");
+}
+
 export async function fetchListings(url) {
   try {
     const getData = {
@@ -19,71 +100,7 @@ export async function fetchListings(url) {
     const json = await response.json();
     console.log(json);
 
-    listingsCont.textContent = "";
-
-    //   all listings
-    json.forEach(function (listing) {
-      const { id, title, description, seller, bids, media } = listing;
-      const listingDate = listing.endsAt.substring(0, 10);
-      const listingTime = listing.endsAt.substring(11, 16);
-      const listingSeller = seller.name;
-      const listingAvatar = seller.avatar;
-      const listingImg = media[0];
-
-      const listingCol = document.createElement("div");
-      listingCol.classList.add("col");
-
-      const listingCard = document.createElement("div");
-      listingCard.classList.add("card");
-
-      const imgCont = document.createElement("div");
-      imgCont.classList.add("card");
-
-      const listingImgs = document.createElement("img");
-      listingImgs.classList.add("img-thumbnail");
-      listingImgs.setAttribute("alt", `${title} listing image`);
-      listingImgs.src = listingImg;
-
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-
-      const h5Title = document.createElement("h5");
-      h5Title.classList.add("card-title");
-      h5Title.textContent = `Item: ${title}`;
-
-      const pDesc = document.createElement("p");
-      pDesc.classList.add("card-text");
-      pDesc.textContent = `Description: ${description}`;
-
-      const pDeadline = document.createElement("p");
-      pDeadline.textContent = `Deadline: ${listingDate} at ${listingTime}`;
-
-      const pSeller = document.createElement("p");
-      pSeller.textContent = `Seller: ${listingSeller}`;
-
-      const sellerAvatar = document.createElement("img");
-      sellerAvatar.classList.add("listingAvatar");
-      sellerAvatar.setAttribute("alt", `${listingSeller}'s avatar`);
-      sellerAvatar.src = listingAvatar;
-
-      const bidLink = document.createElement("a");
-      bidLink.classList.add("btn", "btn-primary");
-      bidLink.href = `listing.html?id=${id}`;
-      bidLink.textContent = "See bids / Add bid";
-
-      listingsCont.appendChild(listingCol);
-      listingCol.appendChild(listingCard);
-      listingCard.appendChild(listingImgs);
-      listingCard.appendChild(cardBody);
-      cardBody.appendChild(h5Title);
-      cardBody.appendChild(pDesc);
-      cardBody.appendChild(pDeadline);
-      cardBody.appendChild(pSeller);
-      cardBody.appendChild(bidLink);
-      pSeller.appendChild(sellerAvatar);
-    });
-
-    loader.classList.add("hide");
+    getListings(json, listingsCont);
   } catch (error) {
     console.log(error);
   }
