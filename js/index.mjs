aeab909b3cad331cfe1a89aeb3bf827eada5cac7
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-05-23 00:17:18 +0200

    impoprted bootstrap

--- js/index.mjs
+++ js/index.mjs
@@ -1,5 +1,5 @@
-// import "bootstrap";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { getListingsURL, listingForm } from "./modules/variables/variables.mjs";
 import { fetchListings } from "./modules/functions/listings/getListings.mjs";
 import { createListing } from "./modules/functions/listings/createListing.mjs";
